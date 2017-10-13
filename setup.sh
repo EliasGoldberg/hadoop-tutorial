c677a09bd68f42da95c96dfd41bd9af778dd5c32
Jeremy Goldberg <elias.goldberg@gmail.com>
2017-10-13 16:04:55 -0400

    Created a script to configure a new Unbuntu 16.04 server via ssh.

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This script automatically makes the changes recommended in DigitalOcean's 
+# "Initial Server Setup with Ubuntu 16.04" article hosted at
+# digitalocean.com/community/tutorials/initial-server-setup-with-ubuntu-16-04.
+# It additionally prevents the login of root over ssh.  The arguments are the 
+# name of the user to create and the contents of the public key file you will
+# be using to login.
+# Replace $remotehost in the following command with the IP address of your
+# remote server (or set the remotehost environment variable) to create a user 
+# with your own user name and using the public key found in your id_rsa.pub.
+# ssh root@$remotehost 'bash -s' < setup.sh $(whoami) "$(<~/.ssh/id_rsa.pub)"
+
+username=$1
+publickey="$2 $3 $4"
+adduser --disabled-password --gecos "" $username 
+usermod -aG sudo $username
+mkdir /home/$username/.ssh
+touch /home/$username/.ssh/authorized_keys
+echo $publickey > /home/$username/.ssh/authorized_keys
+chown -R $username /home/$username/.ssh
+chmod 700 /home/$username/.ssh
+chmod 600 /home/$username/.ssh/authorized_keys
+echo "$username ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
+systemctl reload sshd
+ufw allow OpenSSH
+ufw --force enable
+sed -i 's/^PermitRootLogin yes$/PermitRootLogin no/' /etc/ssh/sshd_config
